d62a35c001b30f40633f9613678dd820ba2708c9
Dimitrios Piliouras <jimpil1985@gmail.com>
2016-01-18 23:13:40 +0000

    add github :url in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject fudje "0.7.9-SNAPSHOT"
   :description "A unit-testing library which provides features very similar to midje for a fraction of the code.
 Fudje was designed to be significantly less 'magical' than midje, and to be AOT friendly (as opposed to midje)."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jimpil/fudje"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
