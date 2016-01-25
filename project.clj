a2d0d7a571d805b68c286de0f2017f3b5d90b37b
Dimitrios Piliouras <jimpil1985@gmail.com>
2016-01-25 18:12:48 +0000

    mark next dev iteration 0.8.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fudje "0.8.6"
+(defproject fudje "0.8.7-SNAPSHOT"
   :description "A small unit-testing library heavily inspired by midje."
   :url "https://github.com/jimpil/fudje"
   :license {:name "Eclipse Public License"
